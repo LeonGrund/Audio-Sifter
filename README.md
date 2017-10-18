d2ef17870f7ac479cb25422009b358f1b51444ce
Andrew Willette <andrewwillette@Andrews-MacBook-Pro.local>
2017-10-18 13:00:25 -0500

    updated readme with pip install details... more to come...

---
+++ README.md
@@ -0,0 +1,7 @@
+# Audio-Sifter
+Packages that need to be installed :
+
+pip install python
+pip install madmom
+pip install numpy
+pip install pydub
--- sifter.py
+++ sifter.py
@@ -11,18 +11,12 @@ import wave
 from pydub import AudioSegment
 
 
-
-
-if __name__ == '__main__':
-    #print("This only executes when %s is executed rather than imported" % __file__)
-    #print "hello world!!"
-    #data, samplerate = sf.read('DrakeFireIce.wav')
-
-
-    #generate
+#GENERATES file 'CutUpSoundFiles' in current directory, which contains 4 .wav files of the audio split up 
+def cutUpQuarters(fileName):
+        #generate
     proc = CRFBeatDetectionProcessor(fps=100)
     proc
-    act = RNNBeatProcessor()(sys.argv[1])
+    act = RNNBeatProcessor()(fileName)
     beatSeconds = proc(act)
 
 
@@ -31,21 +25,30 @@ if __name__ == '__main__':
     if not os.path.exists('CutUpSoundFiles'):
         os.makedirs('CutUpSoundFiles')
 
-    #Start at beginning of beat detection
+    #Generate 
     totalBeats = beatSeconds.size - 1
     halfPoint = totalBeats / 2
     quarterPoint = halfPoint / 2
     threeQuarterPoint = quarterPoint * 3
+
+    #Start at beginning of beat detection
     areasToSplit = [0, quarterPoint, halfPoint, threeQuarterPoint, totalBeats]
-    print areasToSplit
+
+
     for x in range ( 0, len(areasToSplit)-1):
         t1 = (beatSeconds[areasToSplit[x]]*1000)
-        print 't1 is ' + str(t1)
         t2 = (beatSeconds[areasToSplit[x+1]]*1000)
-        print 't2 is ' + str(t2)
-        newAudio = AudioSegment.from_wav(sys.argv[1])
+        
+        newAudio = AudioSegment.from_wav(fileName)
         newAudio = newAudio[t1:t2]
 
 
         newAudio.export('CutUpSoundFiles/newerSong%(number)03d.wav' % {"number": x}, format="wav")
-    #proc(file_name) returns probability of beat at freqeuncy of 100 samples per second
+
+
+
+
+if __name__ == '__main__':
+    cutUpQuarters(sys.argv[1])
+
+    
